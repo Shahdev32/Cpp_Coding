4bd00e95ec60527a369ec4e448247862172d2578
shahdev kumar <shahdevkumar.d@gmail.com>
2025-12-02 18:26:23 +0500

    cpp

---
+++ code.cpp
@@ -0,0 +1,196 @@
+// cpp is case sensetive . 
+#include<iostream>
+using namespace std;
+
+int main(){
+//     cout<<"Hello World\n"<<"this is my first program in c++"<<endl;
+//     cout<<"What is your favourite Language.?"<<endl;
+//     cout<<"Thankyou";
+//     cout<<"-----------------------------------------------------"<<endl;
+//     cout<<"****\n"<<"***\n"<<"**\n"<<"*\n";
+//     int a=5;
+//     int b=10;
+//     int c;
+//     cout<<"a ="<<a<<endl;
+//     cout<<"b ="<<b<<endl;
+//     cout<<"c ="<<c; // it will generate random value to it
+//     cout<<"-------------------------------------------------------"<<endl;
+//     int age=20;
+//    // int marks = 34;
+//     char grade ='A';
+//     bool isAdult = true;
+//     double  price = 4.55;
+//     float PI= 3.1462;
+
+//     cout<<age<<" "<<marks<<" "<<grade<<" "<<isAdult<< " "<<endl;
+//     cout<<"size of char"<<sizeof(char)<<endl;
+//     cout<<"size of bool"<<sizeof(bool)<<endl;
+//      cout<<"size of flaot"<<sizeof(float)<<endl;
+//       cout<<"size of double"<<sizeof(double)<<endl;
+
+
+
+//       // single line comment in c++
+    
+    /*
+    
+    this is multiline comment in c++
+    */
+
+//    int marks ;
+//    cout<<"Enter the student marks:"<<endl;
+//    cin>>marks;
+//    cout<<"Student marks are: "<<marks<<endl;
+
+//     return 0;
+
+//   int n;
+//   cout<<"enter the side of square";
+//   cin>>n;
+//   cout<<"Side of Square is:"<<n*n<<endl;
+
+
+    //const side = 5;
+    // side=4;
+
+  // implicit 
+    // cout<<(10/3)<<endl; //3
+    // cout<<(10/3.0)<<endl; // 3.33;
+    // cout<<('A' +0)<<endl;
+    // cout<<('Z' +0)<<endl;
+    // cout<<('a' +0)<<endl;
+    // cout<<('z' +0)<<endl;
+
+
+    // explicit
+    // cout<<(int)('A')<<endl;
+    // float PI = 3.13;
+    // cout<<(int)(PI)<<endl;
+    // cout<<(int)(10/3)<<endl;
+    // cout<<(char)('A'+3);
+
+
+    // cout<<(bool)3+2<<endl;
+    // cout<<(23.5+'A');
+
+
+
+    // int a = 8, b = 3;
+
+    // // Addition
+    // cout << "a + b = " << (a + b) << endl;
+  
+    // // Subtraction
+    // cout << "a - b = " << (a - b) << endl;
+  
+    // // Multiplication
+    // cout << "a * b = " << (a * b) << endl;
+  
+    // // Division
+    // cout << "a / b = " << (a / b) << endl;
+  
+    // // Modulo
+    // cout << "a % b = " << (a % b) << endl;
+  
+    // // Increament
+    // cout << "++a = " << ++a << endl;
+  
+    // // Decrement
+    // cout << "b-- = " << b--;
+  
+
+  
+
+
+
+
+
+    //  int a = 6, b = 4;
+
+    // // Equal operator
+    // cout << "a == b is " << (a == b) << endl;
+  
+    // // Greater than operator
+    // cout << "a > b is " << (a > b) << endl;
+  
+    // // Greater than Equal to operator
+    // cout << "a >= b is " << (a >= b) << endl;
+  
+    // //  Lesser than operator
+    // cout << "a < b is " << (a < b) << endl;
+  
+    // // Lesser than Equal to operator
+    // cout << "a <= b is " << (a <= b) << endl;
+  
+    // // Not equal to operator
+    // cout << "a != b is " << (a != b);
+
+    
+
+
+
+
+
+
+
+//    int a = 6, b = 4;
+
+//     // Logical AND operator
+//     cout << "a && b is " << (a && b) << endl;
+  
+//     // Logical OR operator
+//     cout << "a || b is " << (a || b) << endl;
+  
+//     // Logical NOT operator
+//     cout << "!b is " << (!b);
+
+
+
+    //  int a = 6, b = 4;
+
+    // // Binary AND operator
+    // cout << "a & b is " << (a & b) << endl;
+
+    // // Binary OR operator
+    // cout << "a | b is " << (a | b) << endl;
+
+    // // Binary XOR operator
+    // cout << "a ^ b is " << (a ^ b) << endl;
+
+    // // Left Shift operator
+    // cout << "a << 1 is " << (a << 1) << endl;
+
+    // // Right Shift operator
+    // cout << "a >> 1 is " << (a >> 1) << endl;
+
+    // // One’s Complement operator
+    // cout << "~(a) is " << ~(a);
+    
+
+
+
+
+
+
+
+
+
+
+    //  int a = 3, b = 4;
+
+    // // Conditional Operator
+    // int result = (a < b) ? b : a;
+    // cout << "The greatest number "
+    //       "is " << result;
+
+
+
+    sizeof (char);
+sizeof (var_name);
+
+    return 0;
+
+    
+} 
+
+
